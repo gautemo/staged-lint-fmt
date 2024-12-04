131288a9fb35a81123ff174b38be736a60ffcf6c
Gaute Meek Olsen <gaute.meek@gmail.com>
2024-12-04 22:31:53 +0100

    trim away empty lines

--- src/log.ts
+++ src/log.ts
@@ -1,9 +1,9 @@
 import { blue, red } from '@std/fmt/colors'
 
 export function logInfo(msg: string) {
-  console.log(`${blue('[staged-lint-fmt]')} ${msg}`)
+  console.log(`${blue('[staged-lint-fmt]')} ${msg.trim()}`)
 }
 
 export function logError(msg: string) {
-  console.error(red(`[staged-lint-fmt] ${msg}`))
+  console.error(red(`[staged-lint-fmt] ${msg.trim()}`))
 }
